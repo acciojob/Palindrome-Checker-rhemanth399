f14028144d5fe3d1d38c19ed9cd47ad25b6b0c13
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-01-11 19:13:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // complete the given function
 
 function palindrome(str){
+	// Convert the string to lowercase for case-insensitive comparison
+    str = str.toLowerCase();
 
+    // Use the split, reverse, and join methods to check if the string is the same when reversed
+    return str === str.split('').reverse().join('');
 }
 module.exports = palindrome
